49f98b23dd4cafcc964cca5863522295e79e939c
Azarudeena <a.azarudeen@coats.com>
2020-01-09 12:07:52 +0530

    composition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class ContactList extends React.Component{
+  render(){
+    const people = this.props.contacts
+
+    return <ol>
+        {people.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+    </ol>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList contacts={[
+      {name:'Azar'}
+      ,{name:'Ajju'}
+      ,{name:'deenu'}
+    ]}/>
+     <ContactList contacts={[
+      {name:'Jammy'}
+      ,{name:'Jammu'}
+      ,{name:'Chan'}
+    ]}/>
     </div>
   );
 }
